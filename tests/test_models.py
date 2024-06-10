f86ed8c15a7a05448c5bc9b05a7815c245476bf7
mawiav08 <mawiav008@gmail.com>
2024-06-10 15:47:24 +0300

    complete code challenge

--- tests/test_models.py
+++ tests/test_models.py
@@ -14,7 +14,7 @@ class TestModels(unittest.TestCase):
 
     def test_magazine_creation(self):
         magazine = Magazine(1, "Tech Weekly")
-        self.assertEqual(magazine.name, "Tech Weekly")
+        self.assertEqual(magazine.name, "Tech Weekly" , "AI")
 
 if __name__ == "__main__":
     unittest.main()
